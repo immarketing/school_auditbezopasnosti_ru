e162a300c42d45ce361516c0e99a22550493297e
immarketing <immarketing@mail.ru>
2016-02-27 00:53:12 +0300

    111

--- js.src/school.js
+++ js.src/school.js
@@ -1,3 +1,14 @@
 /**
  * Created by AVGorbunov on 25.02.2016.
  */
+
+$(document).ready(function () {
+    $(".navbar").on("activate.bs.scrollspy", function () {
+        var x = $(".nav li.active > a");
+        //$("#demo").empty().html("You are currently viewing: " + x);
+        console.log(x.text());
+        console.log(x.length);
+
+        $("a.navbar-brand").css("visibility", "visible");
+    })
+});
